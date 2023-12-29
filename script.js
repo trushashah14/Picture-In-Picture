540a1adeb60f02d0c5756c8abfff7419b769b776
Trusha Shah <trushashah1402@gmail.com>
2023-12-30 01:37:05 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('testing');
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select media stream , pass to video element, then play
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (error) {
+        // catch error
+    }
+}
+
+button.addEventListener('click', async () => {
+    // Disable the button
+    button.disabled = true;
+    // Start Picture in picture
+    await videoElement.requestPictureInPicture();
+    // Reset button
+    button.disabled = false;
+});
+
+// On Load
+selectMediaStream();
